8de772ff7d34baa772e7599e8b976bd3a1b42cc1
atmonshi <atmonshi@users.noreply.github.com>
2024-03-29 09:05:13 +0000

    Fix styling

--- src/Filament/ChaosResource/ChaosForms.php
+++ src/Filament/ChaosResource/ChaosForms.php
@@ -23,49 +23,49 @@ class ChaosForms
 
                 Grid::make()
                     ->schema([
-                    ...$sideSections,
-                    Section::make(__('zeus-chaos::core.record_info'))
-                        ->columns(1)
-                        ->compact()
-                        ->schema([
-                            Placeholder::make('created_at')
-                                ->label(__('zeus-chaos::core.created_at'))
-                                ->content(fn ($record): string => $record?->created_at
-                                    ? $record->created_at->translatedFormat('Y/m/d - h:i A')
-                                    : '-'),
+                        ...$sideSections,
+                        Section::make(__('zeus-chaos::core.record_info'))
+                            ->columns(1)
+                            ->compact()
+                            ->schema([
+                                Placeholder::make('created_at')
+                                    ->label(__('zeus-chaos::core.created_at'))
+                                    ->content(fn ($record): string => $record?->created_at
+                                        ? $record->created_at->translatedFormat('Y/m/d - h:i A')
+                                        : '-'),
 
-                            PopoverForm::make('created_by')
-                                ->formatStateUsing(fn ($record) => $record?->createdBy?->name)
-                                ->placement('right')
-                                ->content(fn ($record) => view('zeus-chaos::tables.columns.popover-user-card', [
-                                    'user' => $record?->createdBy,
-                                    'column' => 'created-by',
-                                    'record' => $record,
-                                ]))
-                                ->label(__('zeus-chaos::core.created_by')),
+                                PopoverForm::make('created_by')
+                                    ->formatStateUsing(fn ($record) => $record?->createdBy?->name)
+                                    ->placement('right')
+                                    ->content(fn ($record) => view('zeus-chaos::tables.columns.popover-user-card', [
+                                        'user' => $record?->createdBy,
+                                        'column' => 'created-by',
+                                        'record' => $record,
+                                    ]))
+                                    ->label(__('zeus-chaos::core.created_by')),
 
-                            Placeholder::make('updated_at')
-                                ->label(__('zeus-chaos::core.updated_at'))
-                                ->content(fn ($record): string => $record?->updated_at
-                                    ? $record->updated_at->translatedFormat('Y/m/d - h:i A')
-                                    : '-'),
+                                Placeholder::make('updated_at')
+                                    ->label(__('zeus-chaos::core.updated_at'))
+                                    ->content(fn ($record): string => $record?->updated_at
+                                        ? $record->updated_at->translatedFormat('Y/m/d - h:i A')
+                                        : '-'),
 
-                            PopoverForm::make('updated_by')
-                                ->formatStateUsing(fn ($record) => $record?->updatedBy?->name)
-                                ->placement('right')
-                                ->content(fn ($record) => view('zeus-chaos::tables.columns.popover-user-card', [
-                                    'user' => $record?->updatedBy,
-                                    'column' => 'updated-by',
-                                    'record' => $record,
-                                ]))
-                                ->label(__('zeus-chaos::core.updated_by')),
-                        ])
-                        ->icon('tabler-info-circle-filled')
-                        ->collapsible()
-                        ->visible(function (string $operation) use ($form, $sideSections) {
-                            return ! empty($sideSections) && ($operation === 'edit' && (new $form->model)->usesTimestamps());
-                        }),
-                ])
+                                PopoverForm::make('updated_by')
+                                    ->formatStateUsing(fn ($record) => $record?->updatedBy?->name)
+                                    ->placement('right')
+                                    ->content(fn ($record) => view('zeus-chaos::tables.columns.popover-user-card', [
+                                        'user' => $record?->updatedBy,
+                                        'column' => 'updated-by',
+                                        'record' => $record,
+                                    ]))
+                                    ->label(__('zeus-chaos::core.updated_by')),
+                            ])
+                            ->icon('tabler-info-circle-filled')
+                            ->collapsible()
+                            ->visible(function (string $operation) use ($form, $sideSections) {
+                                return ! empty($sideSections) && ($operation === 'edit' && (new $form->model)->usesTimestamps());
+                            }),
+                    ])
                     ->visible(function (string $operation) use ($sideSections, $form) {
                         return ! empty($sideSections) && ($operation === 'edit' && (new $form->model)->usesTimestamps());
                     })
